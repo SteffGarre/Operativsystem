ed3a47c21705bd12f8b6798f742fdf45da67c5f7
SteffGarre <61310653+SteffGarre@users.noreply.github.com>
2021-12-01 00:07:12 +0100

    more comments

--- MallocAndFree/bench.c
+++ MallocAndFree/bench.c
@@ -5,21 +5,27 @@
 #include <time.h>
 #include "random.h"
 #include "dlmall.h"
-#define BUFFER 100 
-#define MAX_SIZE 100
+#define TEST_CONST 100 
 
 void allocate(int allocs){
-
-  void *buffer[BUFFER];
-  for(int i = 0; i < BUFFER; i++){
-    buffer[i] = NULL;
+  
+  // create an array of void ptrs and set them all to null.
+  //This will allow a sort of randomizer between calling dfree() and dalloc()
+  //Note: there is a higher probalistic that the dalloc() func is called.
+  void *test_const[TEST_CONST];
+  for(int i = 0; i < TEST_CONST; i++){
+    test_const[i] = NULL;
   }
 
+  //allocs will initially be = TEST_CONST, but will increase in size until
+  //it reaches the requested # of allocation from main!
   for(int i = 0; i < allocs; i++){
-    int index = rand() % BUFFER;
 
-    if(buffer[index] != NULL){
-      dfree(buffer[index]);
+    //randomize a number between 0 and TEST_CONST
+    int index = rand() % TEST_CONST;
+    
+    if(test_const[index] != NULL){
+      dfree(test_const[index]);
     }
 
     size_t size = (size_t)request();
@@ -31,16 +37,22 @@ void allocate(int allocs){
       return;
     }
 
-    buffer[index] = memory;
-    /* writing to the memory so we know it exists */
+    //write to array, increasing the chance that dfree() is called
+    test_const[index] = memory;
+
+    // writing to the memory to make sure that it exists
     *memory = 123;
   }
 }
 
-void checkFreeListLength(int allocs){
+
+//Starts with TEST_CONST number of allocations until we reach the requested # of allocation from main! 
+//Increases by +10 allocations for every iteration
+//Checks length of flist for every iteration
+void check_flist_length(int allocs){
   printf("** Checking length of flist **\n#Allocations\tflistLength\n");
   int sum = 0;
-  for(int i = BUFFER; i < allocs+1; i+= 10){
+  for(int i = TEST_CONST; i < allocs+1; i+= 10){
     init();
     allocate(i);
     freelistlength(i);
@@ -49,11 +61,15 @@ void checkFreeListLength(int allocs){
   printf("\n");
 }
 
+
+//Starts with TEST_CONST number of allocations until we reach the requested # of allocation from main! 
+//Increases by +10 allocations for every iteration
+//checks time performane vs # of allocations for every iteration
 void checkDalloc(int allocs){
   printf("** Testing of time performance vs number of allocations **\n#Allocations\tTime(ms)\n");
   clock_t time_start, time_stop;
   double timeAlloc = 0;
-  for(int i = BUFFER; i < allocs+1; i+= 10){
+  for(int i = TEST_CONST; i < allocs+1; i+= 10){
     init();
     time_start = clock();
     allocate(i);
@@ -75,6 +91,6 @@ int main(int argc, char const *argv[]) {
 
   int allocs = atoi(argv[1]);
   checkDalloc(allocs);
-  checkFreeListLength(allocs);
+  check_flist_length(allocs);
   return 0;
 }
