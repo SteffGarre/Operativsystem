ab3f56863a0b5ed5c5871ae46928ba9a48f3b2d8
SteffGarre <61310653+SteffGarre@users.noreply.github.com>
2021-12-11 13:10:22 +0100

    added Mutex to threads

--- GreenThreads/green.h
+++ GreenThreads/green.h
@@ -1,7 +1,6 @@
 #include <sys/ucontext.h>
 
 typedef struct green_t{
-
     ucontext_t *context;
     void *(*fun)(void*);
     void *arg;
@@ -15,6 +14,13 @@ typedef struct green_cond_t{
     green_t *queue;
 } green_cond_t;
 
+typedef struct green_mutex_t{
+    volatile int taken;
+    //handle the list
+    struct green_t *susp; 
+
+} green_mutex_t;
+
 int green_create(green_t *thread, void *(*fun)(void *), void *arg);
 int green_yield();
 int green_join(green_t *thread, void **val);
@@ -23,3 +29,6 @@ void green_cond_init(green_cond_t *cond);
 void green_cond_wait(green_cond_t *cond);
 void green_cond_signal(green_cond_t *cond);
 
+int green_mutex_init(green_mutex_t *mutex);
+int green_mutex_lock(green_mutex_t *mutex);
+int green_mutex_unlock(green_mutex_t *mutex);
